260d633d926ce20f837af0168decac98d12bffc3
yjleecue <123649978+yjleecue@users.noreply.github.com>
2023-02-27 23:08:56 +0900

    Add files via upload

---
+++ baekjoon.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class baekjoon {
+    public static void main(String[] args) {
+        String S = "baekjoon";
+        String s = S;
+        String[] array = new String[S.length()];
+
+        for (int i = 0; i < S.length(); i++) {
+            //System.out.println(s.substring(1));
+            array[i] = s.substring(1);
+            System.out.println(array[i]);
+            s = array[i];
+        }
+
+
+        // List<String> list = new ArrayList<>(Arrays.asList(s));
+    }
+}
